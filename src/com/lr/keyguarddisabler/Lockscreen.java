18e586195ae8a6780d0158c29157e03c1bd59bba
LucasR93 <l93cont@gmail.com>
2014-08-26 10:15:23 +0200

    Added preference to enable/disable logging. Implemented custom logging method. log() should be used instead of direct call to XposedBridge.log()

--- src/com/lr/keyguarddisabler/Lockscreen.java
+++ src/com/lr/keyguarddisabler/Lockscreen.java
@@ -24,6 +24,13 @@ public class Lockscreen implements IXposedHookLoadPackage, IXposedHookZygoteInit
 		prefs = new XSharedPreferences("com.lr.keyguarddisabler");
 		prefs.makeWorldReadable();
 	}
+	
+	private void log(String message){
+		prefs.reload();
+		if(prefs.getBoolean("logtofile", false)){
+			XposedBridge.log(message);
+		}
+	}
 
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 
@@ -60,7 +67,7 @@ public class Lockscreen implements IXposedHookLoadPackage, IXposedHookZygoteInit
 		// Android 4.4
 		if (lpparam.packageName.contains("android.keyguard")) {
 
-			XposedBridge.log("Keyguard Disabler: Loading Kitkat specific code");
+			log("Keyguard Disabler: Loading Kitkat specific code");
 
 			hookGetSecurityMode(lpparam, "com.android.keyguard.KeyguardSecurityModel$SecurityMode", "com.android.keyguard.KeyguardSecurityModel", "Android 4.4");
 
@@ -69,21 +76,21 @@ public class Lockscreen implements IXposedHookLoadPackage, IXposedHookZygoteInit
 		// Android < 4.4
 		else if (lpparam.packageName.contains("android.internal")) {
 
-			XposedBridge.log("Keyguard Disabler: Loading Jellybean specific code");
+			log("Keyguard Disabler: Loading Jellybean specific code");
 			hookGetSecurityMode(lpparam, "com.android.internal.policy.impl.keyguard.KeyguardSecurityModel$SecurityMode", "com.android.internal.policy.impl.keyguard.KeyguardSecurityModel", "Android < 4.4");
 		}
 
 		// HTC
 		else if (lpparam.packageName.contains("com.htc.lockscreen")) {
 
-			XposedBridge.log("Keyguard Disabler: Loading HTC specific code");
+			log("Keyguard Disabler: Loading HTC specific code");
 
 			try {
-				XposedBridge.log("Keyguard Disabler: Loading HTC specific code - trying new Sense 5.x Code - thanks to XDA @Sirlatrom");
+				log("Keyguard Disabler: Loading HTC specific code - trying new Sense 5.x Code - thanks to XDA @Sirlatrom");
 				hookGetSecurityMode(lpparam, "com.htc.lockscreen.HtcKeyguardSecurityModel$SecurityMode", "com.htc.lockscreen.HtcKeyguardSecurityModel", "HTC Sense 5.x");
 				
 			} catch (ClassNotFoundError ex) {
-				XposedBridge.log("Keyguard Disabler: Loading HTC specific code - trying new Sense 6 Code - thanks to XDA @Sirlatrom");
+				log("Keyguard Disabler: Loading HTC specific code - trying new Sense 6 Code - thanks to XDA @Sirlatrom");
 				hookGetSecurityMode(lpparam, "com.htc.lockscreen.keyguard.KeyguardSecurityModel$SecurityMode", "com.htc.lockscreen.keyguard.KeyguardSecurityModel",
 						"HTC Sense 6");
 			}
@@ -94,7 +101,7 @@ public class Lockscreen implements IXposedHookLoadPackage, IXposedHookZygoteInit
 			final String nestedClassName, final String outerClassName,
 			final String variant) throws ClassNotFoundError {
 		securityModeEnum = XposedHelpers.findClass(nestedClassName, lpparam.classLoader);
-		XposedBridge.log("SecurityEnum (" + variant + ") found and parsed");
+		log("SecurityEnum (" + variant + ") found and parsed");
 		XposedHelpers.findAndHookMethod(outerClassName, lpparam.classLoader, "getSecurityMode", new XC_MethodHook() {
 		
 			@Override
@@ -103,13 +110,13 @@ public class Lockscreen implements IXposedHookLoadPackage, IXposedHookZygoteInit
 				if (prefs.getString("lockscreentype", "slide").equals("slide")) {
 					TelephonyManager manager = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
 					if (manager.getSimState() == TelephonyManager.SIM_STATE_PIN_REQUIRED) {
-						XposedBridge.log("SIM PIN REQUIRED (" + variant + ")");
+						log("SIM PIN REQUIRED (" + variant + ")");
 						param.setResult(XposedHelpers.getStaticObjectField(securityModeEnum, "SimPin"));
 					} else if (manager.getSimState() == TelephonyManager.SIM_STATE_PUK_REQUIRED) {
-						XposedBridge.log("SIM PUK REQUIRED (" + variant + ")");
+						log("SIM PUK REQUIRED (" + variant + ")");
 						param.setResult(XposedHelpers.getStaticObjectField(securityModeEnum, "SimPuk"));
 					} else {
-						XposedBridge.log("Returning NO Security (" + variant + ")");
+						log("Returning NO Security (" + variant + ")");
 						param.setResult(XposedHelpers.getStaticObjectField(securityModeEnum, "None"));
 					}
 				}
